d5501f23d1d172ae05a6c8b708b71e5896b8b4d9
ggorsontanguy <guillaume@gorson-tanguy.net>
2015-09-04 03:17:14 +0200

    Ajout VehiculeBiColor + TU KO

---
+++ src/VehiculeBiColor.java
@@ -0,0 +1,9 @@
+
+public class VehiculeBiColor extends Vehicule {
+
+	public VehiculeBiColor(Color color1, Color color2) {
+		super(color1);
+		carroserie.color = color2;
+	}
+
+}
--- src/VehiculeTest.java
+++ src/VehiculeTest.java
@@ -18,4 +18,17 @@ public class VehiculeTest {
 		assertSame(color, vehicule.carroserie.color);
 	}
 		
+	@Test
+	public final void testVehiculeBiColor() {
+		Color color = Color.BLEU;
+		Vehicule vehicule = new VehiculeBiColor(color, Color.ROUGE);
+		assertSame(color, vehicule.roue.color);
+		assertSame(color, vehicule.carroserie.color);
+		
+		color = Color.ROUGE;
+		vehicule = new VehiculeBiColor(color, Color.BLEU);
+		assertSame(color, vehicule.roue.color);
+		assertSame(color, vehicule.carroserie.color);
+	}
+
 }
